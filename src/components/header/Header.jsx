606c1cd445a4db4d4e79e10c3503652c9d474db5
hammer200911 <jdaniel231@gmail.com>
2022-05-20 14:09:52 -0300

    header e install react-icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Ola! Eu sou </h5>
+        <h2>João Daniel</h2>
+        <h5 className="text-light">desenvolvedor Font-End</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className="scrool__down" >Scrool Down</a>
+      </div>
+    </header>
   )
 }
 
